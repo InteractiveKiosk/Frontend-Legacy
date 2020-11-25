f60369e69d102939e48575d7999e9fd126d7db91
heptacode <admin@hyunwoo.kim>
2020-11-25 20:53:23 +0900

    Update Home

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,8 +1,12 @@
 <template>
-  <div class="home">
-    <STT></STT>
-    <TTS></TTS>
-  </div>
+	<div class="home">
+		<h1>환영합니다</h1>
+		<img src="https://firebasestorage.googleapis.com/v0/b/interactive-kiosk.appspot.com/o/undraw_Payments_re_77x0.svg?alt=media&token=9c846d06-e185-4383-a40c-9f19511cd747" alt="Payments Image" />
+		<STT></STT>
+		<TTS></TTS>
+		<button>시작하기</button>
+		<small>이어폰을 꽂으면 목소리로 주문할 수 있습니다.</small>
+	</div>
 </template>
 
 <script lang="ts">
@@ -11,7 +15,24 @@ import TTS from "@/components/TTS.vue";
 import { Component, Vue } from "vue-property-decorator";
 
 @Component({
-  components: { STT, TTS },
+	components: { STT, TTS },
 })
 export default class Home extends Vue {}
 </script>
+
+<style lang="scss" scoped>
+.home {
+	display: flex;
+	flex-direction: column;
+	justify-content: center;
+	align-self: center;
+
+	max-width: 720px;
+
+	color: $disabled-color;
+
+	img {
+		width: 300px;
+	}
+}
+</style>

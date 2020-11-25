84ae170b3b1e77cd0f4ab11b2ecb0593c0093078
heptacode <admin@hyunwoo.kim>
2020-11-25 20:45:38 +0900

    Add Style

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,28 @@
 <template>
 	<div id="app">
-		<router-view />
+		<router-view class="router" />
 	</div>
 </template>
 
 <style lang="scss">
+@import "variables.scss";
+@import url(//spoqa.github.io/spoqa-han-sans/css/SpoqaHanSans-kr.css);
+
 * {
 	margin: 0;
 	padding: 0;
 	box-sizing: border-box;
+	font-family: "Spoqa Han Sans";
 }
-html {
-	background-color: white;
+#app {
+	width: 100%;
+	height: 100vh;
+	display: flex;
+	justify-content: center;
+}
+.router {
+	height: 100%;
+	width: 100%;
+	overflow: hidden;
 }
 </style>

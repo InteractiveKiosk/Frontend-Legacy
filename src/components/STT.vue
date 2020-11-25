0166a05c6c3fc909303bae0a6c6be00e2ab5d232
Andy-0414 <pjh8667@naver.com>
2020-11-25 17:29:50 +0900

    Update trans1 & trans2 & electron

--- src/components/STT.vue
+++ src/components/STT.vue
@@ -3,7 +3,8 @@
 		<audio :src="url" controls></audio>
 		<button @click="start">start</button>
 		<button @click="stop">stop</button>
-		<button @click="trans">trans</button>
+		<button @click="trans1">trans1</button>
+		<button @click="trans2">trans2</button>
 	</div>
 </template>
 
@@ -31,7 +32,7 @@ export default class STT extends Vue {
 
 		this.mediaRecorder = new MediaRecorder(stream);
 		this.mediaRecorder.addEventListener("dataavailable", (ev: any) => {
-			this.blob = new Blob([ev.data], { type: "audio/mp3" });
+			this.blob = new Blob([ev.data], { type: "application/octet-stream" });
 			this.url = URL.createObjectURL(this.blob);
 		});
 	}
@@ -44,8 +45,54 @@ export default class STT extends Vue {
 		this.mediaRecorder.stop();
 	}
 
-	trans() {
-		// axios.post("https://naveropenapi.apigw.ntruss.com/recog/v1/stt",)
+	async trans1() {
+		console.log("변환 시작");
+		try {
+			let result = await axios.post("https://naveropenapi.apigw.ntruss.com/recog/v1/stt", this.blob, {
+				params: {
+					lang: "Kor",
+				},
+				headers: {
+					"Content-Type": "application/octet-stream",
+					"X-NCP-APIGW-API-KEY-ID": "",
+					"X-NCP-APIGW-API-KEY": "",
+				},
+				withCredentials: true,
+			});
+			console.log("변환 종료");
+			console.log(result);
+		} catch (err) {
+			console.log(err);
+		}
+	}
+	async trans2() {
+		console.log("변환 시작");
+		let blobToFile = (theBlob: Blob, fileName: string): File => {
+			var b: any = theBlob;
+			//A Blob() is almost a File() - it's just missing the two properties below which we will add
+			b.lastModifiedDate = new Date();
+			b.name = fileName;
+
+			//Cast to a File() type
+			return <File>theBlob;
+		};
+		try {
+			let result = await axios.post("https://naveropenapi.apigw.ntruss.com/recog/v1/stt", blobToFile(this.blob!, "test"), {
+				params: {
+					lang: "Kor",
+				},
+				headers: {
+					"Content-Type": "application/octet-stream",
+					"X-NCP-APIGW-API-KEY-ID": "",
+					"X-NCP-APIGW-API-KEY": "",
+				},
+				withCredentials: true,
+			});
+			console.log("변환 종료");
+			console.log(result);
+		} catch (err) {
+			console.log(err);
+		}
 	}
 }
 </script>
